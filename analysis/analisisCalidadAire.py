db017434454332b2b64b7c80289480f753f871da
Adrian Jaramillo <adrianjagudelo@gmail.com>
2024-06-19 17:38:05 -0500

    actualizacion con graficas

--- analysis/analisisCalidadAire.py
+++ analysis/analisisCalidadAire.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 from generators.generadorICA import generarDatosICA
 
@@ -8,5 +10,12 @@ def construirDataIcCA():
     dataFrameICA = pd.DataFrame(datosICA, columns=['comuna','ica','fecha','id'])
     dataFrameICA.to_csv('datosica.csv', index=False)
     print(dataFrameICA)
+
+
+    #generando grafica de los datos por comuna
+    datosOrdenadosPorComuna = dataFrameICA.groupby('comuna')['ica'].mean()
+    plt.figure(figsize=(20,20))
+    datosOrdenadosPorComuna.plot(kind='bar', color='green')
+    plt.show()
 
 construirDataIcCA()
